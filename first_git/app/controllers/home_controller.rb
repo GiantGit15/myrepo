2316072973525635167dfbabe81f1eb0c1301094
kevinwirth15 <kevinwirth15@yahoo.com>
2015-06-17 19:26:43 +0300

    first_git

---
+++ first_git/app/controllers/home_controller.rb
@@ -0,0 +1,21 @@
+class HomeController < ApplicationController
+
+  protect_from_forgery with: :null_sessions
+
+  def settings
+    @event = Event.latest
+  end
+
+  def signed_in
+  end
+
+  def authentication_failure
+  end
+
+  def admin
+    SyncEvents.call # Ensure we can see all the events n the directory.
+    @events = Event.all
+    @event = Event.latest
+  end
+
+end
